65776a3b2bcd8c27b8a5c707bcd19ebe571e1b6c
Daniel Covill <daniel.covill@gmail.com>
2017-09-05 11:50:57 -0600

    Fix issue in tsconfig

---
+++ settings.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.__esModule = true;
+var Settings = /** @class */ (function () {
+    function Settings() {
+    }
+    Settings.prototype.settingsInitialized = function () {
+        return new Promise(function (resolve, reject) {
+            resolve(true);
+        });
+    };
+    return Settings;
+}());
+exports.Settings = Settings;
